c6d0d8a9af6d5becad62e698ac16a8e64bc13de1
Islomjon Jamshedov <jonjamshedov@gmail.com>
2022-12-12 10:58:38 -0600

    Project

--- src/Shape.java
+++ src/Shape.java
@@ -8,8 +8,9 @@
 public interface Shape {
 
     //Create an abstract method called as area and returns a double
-
+    abstract double area();
 
     //Create an abstract method called as perimeter and returns a double
+    abstract double perimeter();
 
 }
--- src/TestShapes.java
+++ src/TestShapes.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -13,34 +14,54 @@ public class TestShapes {
         /*
         This will be our actual program that we request some information from user to create 3 objects
         We will create 1 Rectangle, 1 Square and 1 Circle object
-
         Information to create objects will be as below
          -width of the rectangle = 5.5
          -height of the rectangle = 6
          -side of the square = 2.5
          -radius of the circle = 4
-
-
          Then print each object
          And print area and perimeters of each object
          Find and print the object which has the largest area
-
          EXPECTED OUTPUT:
          Circle{radius=4.0}
          Area of the Circle is = 50.24
          Perimeter of the Circle is = 25.12
-
          Rectangle{width=5.5, height=6.0}
          Area of the Rectangle is = 33.0
          Perimeter of the Rectangle is = 23.0
-
          Square{side=2.5}
          Area of the Square is = 6.25
          Perimeter of the Square is = 10.0
-
          Circle has the largest area as 50.24
          */
 
+        Circle circle = new Circle();
+        circle.setRadius(4.0);
+        System.out.println(circle);
+        System.out.println("Area of the Circle is = " + circle.area());
+        System.out.println("Perimeter of the Circle is = " + circle.perimeter());
+        System.out.println();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(5.5);
+        rectangle.setHeight(6);
+        System.out.println(rectangle);
+        System.out.println("Area of the Rectangle is = " + rectangle.area());
+        System.out.println("Perimeter of the Rectangle is = " + rectangle.perimeter());
+        System.out.println();
+
+        Square square = new Square();
+        square.setSide(2.5);
+        System.out.println(square);
+        System.out.println("Area of the Square is = " + square.area());
+        System.out.println("Perimeter of the Square is = " + square.perimeter());
+        System.out.println();
+
+        if (square.area() > circle.area() && square.area() > rectangle.area())
+            System.out.println("Square has the largest area as " + square.area());
+        else if (circle.area() > square.area() && circle.area() > rectangle.area())
+            System.out.println("Circle has the largest area as " + circle.area());
+        else System.out.println("Rectangle has the largest area as " + rectangle.area());
 
     }
 }
